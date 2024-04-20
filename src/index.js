bcc5638805e18bab5c868ef6fd7c7c6f7f50068c
KeshavTandon <keshavtandon51@gmail.com>
2024-04-20 17:06:43 +0530

    Fix:Folder structure of env

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ const { PORT } = require('./config/server.config');
 
 const app=express();
 
-// app.get('/',(req,res)=>{
-//     res.send("This is for testing /");
-
-// })
-// console.log(`PORT is${PORT}`);
-app.listen(PORT,(req,res)=>{
-    console.log(`Server started at PORT:${PORT}`);
-})
+app.listen(PORT,()=>{
+    console.log(`Server started at PORT : ${PORT}`);
+});
